4597dea3db6337d9eeca0ec91e9496407ec95406
Letrix7 <letrix7@gmail.com>
2024-01-13 05:16:04 +0100

    added functionality for adding items to cart as well as basic ui for displaying those items in cart

--- client/Pages/CartPage.jsx
+++ client/Pages/CartPage.jsx
@@ -15,6 +15,7 @@ export default function CartPage(props) {
   const {pathname, hash, search} = location
   const pathText = pathname.split("/")
   const [cartDetails, setCartDetails] = useState([]);
+  const [totalPrice, setTotalPrice] = useState();
 
   async function fetchCartInfo() {
     if (isUserAuthenticated) {
@@ -27,34 +28,112 @@ export default function CartPage(props) {
           if (res.ok) {
             const data = await res.json();
             setCartDetails(data.items)
+            let sum = 0;
+            for (let i = 0; i <cartDetails.length; i++) {
+              sum += cartDetails[i].itemPrice
+            }
             console.log(cartDetails)
+            console.log(sum)
+            setTotalPrice(sum)
           }
     }
+  }
+
+  const handleQuantityChange = (index, event) => {
+    const newCartDetails = [...cartDetails];
+    const originalPrice = newCartDetails[index].itemPrice / parseInt(event.target.value, 10)  // Assuming itemPrice represents the total price of one item
+    const newQuantity = parseInt(event.target.value, 10);
+  
+    newCartDetails[index].itemPrice = originalPrice * newQuantity;
+  
+    setCartDetails(newCartDetails);
+    calculateTotalPrice(); // Update the total price based on the changes
+  };
+  console.log(cartDetails)
+
+  function calculateTotalPrice() {
+    let sum = 0;
+            for (let i = 0; i <cartDetails.length; i++) {
+              sum += cartDetails[i].itemPrice
+            }
+            setTotalPrice(sum)
   }
 
   useEffect(() => {
     fetchCartInfo()
   }, [])
 
+  useEffect(() => {
+    calculateTotalPrice()
+  }, [cartDetails])
+
   const cartElements = cartDetails.map((item, index) => {
     return (
-      <div style={{display: "flex", maxWidth: "1200px", maxHeight: "100px", alignItems: "center", gap: "10px", backgroundColor: "rgba(0, 0, 0, 0.1)", borderStyle: "solid",
-      borderWidth: "1px"}}>
+      <div className="cart-items-div" key={index}>
         <div>
-          <img src={item.itemImage} style={{maxHeight: "60px", maxWidth: "60px"}}/>
+          <img src={item.itemImage} style={{maxHeight: "100px", maxWidth: "100px"}}/>
         </div>
-        <div style={{height: "100px", width: "1px", backgroundColor: "black"}}></div>
+        <div style={{height: "180px", width: "1px", backgroundColor: "black"}}></div>
         <div style={{display: "flex", flexDirection: "column"}}>
           <h4>{item.itemName}</h4>
-          <h5 style={{fontWeight: "400", fontSize: "0.8rem"}}>{item.itemDescription}</h5>
-          <h5>${item.itemPrice.toFixed(2)}</h5>
+          <h5 style={{fontWeight: "400", fontSize: "0.8rem", maxWidth: "650px"}}>
+          {Array.from(item.itemDescription).map((item, index) => {
+          if (index < 200) {
+            return (
+            item
+            )
+          } else if (index >= 200 && index <= 203) {
+            return (
+              "."
+            )
+          }
+        })}
+          </h5>
+          
+        <div className="cart-item-operations-div">
+          <button>Remove from cart</button>
         </div>
+        </div>
+        <div style={{display: "flex", alignItems: "center", justifyContent: "center", paddingLeft: "140px", flexDirection: "column",
+          gap: "20px"}}
+        >
+          <h3 style={{}}>${item.itemPrice.toFixed(2)}</h3>
+          <select className="cart-quantity-selecter"
+          onChange={(event) => handleQuantityChange(index, event)}
+          >
+            <option value={1}>Qty: 1</option>
+            <option value={2}>Qty: 2</option>
+            <option value={3}>Qty: 3</option>
+            <option value={4}>Qty: 4</option>
+            <option value={5}>Qty: 5</option>
+          </select>
+        </div>
+        
       </div>
     )
   })
   return (
-    <div style={{display: "flex", flexDirection: "column", marginLeft: "200px", marginTop: "100px", gap: "10px"}}>
-      {cartElements}
-  </div>
+    <div className="cart-page-div">
+      <div className="cart-div">
+        <h1 style={{marginRight: "770px"}}>Shopping Cart</h1>
+        <div style={{height: "1px", width: "1000px", background: "linear-gradient(142deg, rgba(2,0,36,1) 0%, rgba(157,24,89,1) 37%, rgba(143,49,159,1) 57%, rgba(18, 61, 182, 1) 100%)"}}></div>
+        {cartElements}
+        <div style={{height: "1px", width: "1000px", background: "linear-gradient(142deg, rgba(2,0,36,1) 0%, rgba(157,24,89,1) 37%, rgba(143,49,159,1) 57%, rgba(18, 61, 182, 1) 100%)"}}></div>
+        <div style={{display: "flex", alignItems: "flex-end", maxHeight: "50px", justifyContent: "flex-end", padding: "20px"}}>
+          <h3 style={{marginLeft: "770px", fontWeight: "400"}}
+          >Subtotal {`(${cartElements?.length != 0 && cartElements.length} items)`}: <b>${totalPrice && totalPrice.toFixed(2)}</b></h3>
+        </div>
+      </div>
+
+      <div className="checkout-from-cart-div">
+        
+        <h4 style={{fontWeight: "400", color: "green"}}>
+          Our company provides <b>100% free shipping</b> anywhere in the world! 
+        </h4 >
+        <h4 style={{fontWeight: "400", color: "green"}}>Dissatisfied? No problem! We provide a 100% money-back-guarantee for our dissatisfied customers</h4>
+        <h3 style={{fontWeight: "400"}}>Subtotal {`(${cartElements?.length != 0 && cartElements.length} items)`}: <b>${totalPrice && totalPrice.toFixed(2)}</b></h3>
+        <button>Proceed to checkout</button>
+      </div>
+    </div>
   )
 }
