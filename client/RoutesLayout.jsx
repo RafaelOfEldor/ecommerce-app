1e38d4d4d2b3092660ff946faf03b201ddcd0154
Letrix7 <letrix7@gmail.com>
2024-01-18 18:48:34 +0100

    started on products page and item details page

--- client/RoutesLayout.jsx
+++ client/RoutesLayout.jsx
@@ -6,6 +6,7 @@ import LoginPage from "./Pages/LoginPage";
 import RegisterPage from "./Pages/RegisterPage";
 import AboutPage from "./Pages/AboutPage";
 import ProtectedRoute from "./Components/ProtectedRoute";
+import ProductComponent from "./Components/ProductComponent";
 import AccountPage from "./Pages/AccountPage";
 import CartPage from "./Pages/CartPage";
 import AccountProfileComponent from "./Pages/account-subpages/ProfileComponent";
@@ -14,6 +15,7 @@ import AccountCancellationsComponent from "./Pages/account-subpages/Cancellation
 import AccountPaymentComponent from "./Pages/account-subpages/PaymentComponent";
 import AccountPurchasesComponent from "./Pages/account-subpages/PurchasesComponent";
 import AccountPrivacyComponent from "./Pages/account-subpages/PrivacyComponent";
+import ProductsPage from "./Pages/ProductsPage";
 
 export default function RoutesPage() {
   return (
@@ -21,6 +23,10 @@ export default function RoutesPage() {
       
       <Route path="/" element={<NavBar />}>
         <Route index element={<HomePage />}/>
+
+        <Route path="/products" element={<ProductsPage />}/>
+        <Route path="/products/product/:itemid" element={<ProductComponent />}/>
+
         <Route path="/login" element={<LoginPage />}/>
         <Route path="/register" element={<RegisterPage />}/>
         <Route path="/about" element={<ProtectedRoute><AboutPage /></ProtectedRoute>}/>
