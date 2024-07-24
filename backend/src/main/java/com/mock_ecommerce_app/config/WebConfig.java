1ec1898fd96c5ceacb1391d63e81fae659b19e75
Letrix7 <letrix7@gmail.com>
2024-07-24 06:34:39 +0200

    local development works with env variables now! The backend environmental variable needs to be set in intellij for now

--- backend/src/main/java/com/mock_ecommerce_app/config/WebConfig.java
+++ backend/src/main/java/com/mock_ecommerce_app/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.mock_ecommerce_app.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
