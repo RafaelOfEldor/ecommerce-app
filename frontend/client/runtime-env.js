721a8eb9adec91fc69cb8b911c59127524d3743b
RafaelOfEldor <letrix7@gmail.com>
2024-07-25 03:26:23 +0200

    makeshift solution where the frontend runtime variable is manually written into the runtime-env.js file once by the user before deployment, instead of being server from nginx from actual environmental variables. Kinda like a single constant declared one place in code before starting

--- frontend/client/runtime-env.js
+++ frontend/client/runtime-env.js
@@ -2,7 +2,7 @@
 (function() {
   console.log('Loading runtime-env.js');
   window.__ENV__ = {
-    BACKEND_API_BASE_URL: "__BACKEND_API_BASE_URL__"
+    BACKEND_API_BASE_URL: "http://backend.mock-ecommerce-namespace:8080"
   };
   console.log('Environment Variables:', window.__ENV__);
 })();
