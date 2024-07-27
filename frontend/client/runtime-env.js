f5c684d8effa394f55959ae14b00c3cd28303db1
RafaelOfEldor <letrix7@gmail.com>
2024-07-27 02:49:46 +0200

    minor bug

--- frontend/client/runtime-env.js
+++ frontend/client/runtime-env.js
@@ -2,7 +2,7 @@
 (function() {
   console.log('Loading runtime-env.js');
   window.__ENV__ = {
-    BACKEND_API_BASE_URL: "http://backend.mock-ecommerce-namespace:8080"
+    BACKEND_API_BASE_URL: "http://localhost:8080"
   };
   console.log('Environment Variables:', window.__ENV__);
 })();
