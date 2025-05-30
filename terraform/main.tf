8036cd92359352bea119da972a1139ccc9982c8d
Revendir <letrix7@gmail.com>
2025-05-31 00:27:38 +0200

    added bucket to store backend state

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,14 @@ terraform {
 provider "aws" {
   region = "eu-west-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "mock-ecommerce-state-bucket"
+    key    = "mock-ecommerce/terraform.tfstate"
+    region = "eu-west-1"
+    encrypt = true
+  }
+}
+
+
